b8e2fb9f8782d383b4092e8802bfe53557685771
eclipse_user <574185505@qq.com>
2019-05-20 14:46:02 +0800

    first git

---
+++ src/main/java/com/ahead/test/One.java
@@ -0,0 +1,9 @@
+package com.ahead.test;
+
+public class One {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/ahead/test/GoodTest.java
@@ -0,0 +1,9 @@
+package com.ahead.test;
+
+public class GoodTest {
+
+	public static void main(String[] args) {
+		System.out.println("hello1");
+	}
+
+}
