1cc093ffc70a195ef074b023e4eddeeadcb54dda
mwei <574185505@qq.com>
2019-05-20 15:27:20 +0800

    提交

--- src/main/java/com/ahead/test/One.java
+++ src/main/java/com/ahead/test/One.java
@@ -4,8 +4,6 @@ public class One {
 
 	public static void main(String[] args) {
 		System.out.println("hello1111111111111");
-		System.out.println("hello1111111111111");
-		System.out.println("hello1111111111111");
 	}
 
 }
